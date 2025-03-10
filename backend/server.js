5bced1474c0ea83908ba9c9b8399b372fec38972
shrutiramenahalli <shrutir456@gmail.com>
2025-03-10 14:00:18 +0000

    Fixed frontend build path

--- backend/server.js
+++ backend/server.js
@@ -87,17 +87,14 @@ app.get("/api/media", async (req, res) => {
   }
 })
 
-app.use(express.static(path.join(__dirname, "frontend/build")))
+app.use(express.static(path.join(__dirname, "build")))
 
 app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "frontend/build", "index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err)
-      }
+  res.sendFile(path.join(__dirname, "build", "index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err)
     }
-  )
+  })
 })
 
 // Start the Server
